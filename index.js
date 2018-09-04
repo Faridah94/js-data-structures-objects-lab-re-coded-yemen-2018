8a480f871068b6c9e69ae5c11249478a4187e758
Farida <faridah.absi@gmail.com>
2018-09-04 09:48:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-   return driver.address=value;
+   return driver[key] = value;
   
 }
